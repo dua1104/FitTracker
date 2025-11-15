82e666eabd60650132133896fbf30a774919ad87
Louay <ninjalouay@gmail.com>
2025-11-15 17:58:24 +0100

    handling login/signup
    # Conflicts:
    #	.idea/gradle.xml
    #	app/build.gradle.kts
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/fittracker/MainActivity.java
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/layout/activity_signup.xml
    #	app/src/main/res/values-night/themes.xml
    #	app/src/main/res/values/themes.xml
    #	gradle/libs.versions.toml
    #	gradle/wrapper/gradle-wrapper.properties
    #	settings.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.fittracker"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.fittracker"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,6 +19,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "FitTracker"
+rootProject.name = "fittracker"
 include(":app")
  
